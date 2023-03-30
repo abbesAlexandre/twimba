2360b183455942c349738802d72978421d4b34bd
KimiWaDare <abbes.alexandre@gmail.com>
2023-03-30 16:13:02 +0200

    Completing design and modal popup

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-import { tweetsData } from '/data.js'
+import { tweetsData } from "/data.js";
+
+const modal = document.getElementsByClassName("modal")[0];
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("tweet-menu")) {
+    let rect = e.target.getBoundingClientRect();
+    modal.classList.toggle("show");
+    
+    if (window.matchMedia("(min-width: 320px)").matches) {
+      modal.style.left = rect.right - 150 + "px";
+      modal.style.top = rect.top + "px";
+    } 
+    if (window.matchMedia("(min-width: 1440px)").matches) {
+      modal.style.left = rect.right - 140 + "px"; 
+      modal.style.top = rect.top + "px";
+    } 
+    if(window.matchMedia("(min-width: 1920px)").matches){
+      modal.style.left = rect.right - 180 + "px";
+      modal.style.top = rect.top + "px";
+    }
+  } else if (!e.target.parentNode.classList.contains("menu")){
+    console.log(e.target);
+    modal.classList.remove("show");
+  }
+});
